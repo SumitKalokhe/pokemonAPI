ac694649d74e0acbf181f17cd8bb2338b3b53a11
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-12-23 21:55:10 +0530

    created new component to show pokemon stats data

---
+++ src/components/pokemonstats/PokemonStats.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './pokemonstats.css'
+
+const PokemonStats = ({stats}) => {
+
+// console.log(stats);
+
+const baseStat= stats.base_stat
+const statName= stats.stat.name
+  return (
+    <div className='pokemon-state-card'>
+        <div className='pokemon-stat-name'>{statName}</div>
+        <div className='pokemon-base-stat'>{baseStat}</div>
+    </div>
+  )
+}
+
+export default PokemonStats
