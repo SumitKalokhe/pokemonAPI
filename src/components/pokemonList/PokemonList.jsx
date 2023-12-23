762773ac45058dc40b7147b33dd4c3b151b6f390
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-12-23 21:15:32 +0530

    created page to show pokemon cards

--- src/components/pokemonList/PokemonList.jsx
+++ src/components/pokemonList/PokemonList.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
+import './pokemonlist.css'
 
-const PokemonList = ({ name, url, details, index, img}) => {
+const PokemonList = ({ name, url, details, id, img, stats, types }) => {
+
+    const pokemonType = types.map((type) => {
+        
+        return type.type.name
+    })
 
     return (
-        <div className='pokemon-card'>
-            <div>{name}</div>
-            <div>{url}</div>
-            <img src={img} alt={name} />
-        </div>
+        <NavLink to={`/${id}`}>
+            <div className='pokemon-card'>
+                <img className='pokemon-card-img' src={img} alt={name} />
+                <div className='pokemon-card-info'>
+                    <div className='pokemon-card-name'>
+                        {name}
+                    </div>
+                    <div className='pokemon-type-box'>
+                        {pokemonType.map((type, index) => {
+                            return <div key={index} className='pokemon-type'>{type}</div>
+                        })}
+                    </div>
+                </div>
+            </div>
+         </NavLink>
     )
 }
 
