839029e1110a624296316904eab7d863c608f004
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-12-23 21:55:53 +0530

    got data of single pokemons stats and passed it as props to pokemonStats component

--- src/components/singlepokemonmodel/PokemonModel.jsx
+++ src/components/singlepokemonmodel/PokemonModel.jsx
@@ -1,20 +1,30 @@
 import React, { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { PokemonContext } from '../context/PokemonContext'
+import PokemonStats from '../pokemonstats/PokemonStats'
+import "./pokemonModel.css"
 
-const API="https://pokeapi.co/api/v2/pokemon"
+const API = "https://pokeapi.co/api/v2/pokemon"
 const PokemonModel = () => {
 
-  const {getPokemonModel}= useContext(PokemonContext)
-  const {id}= useParams()
-  console.log("This is id: ",id);
-  console.log(`this is url of model: ${API}/${id}` );
+  const { getPokemonModel } = useContext(PokemonContext)
+  const { id } = useParams()
+  const { pokemonStats } = useContext(PokemonContext)
 
-  useEffect(()=>{
+  // console.log("This is id: ",id);
+  // console.log(`this is url of model: ${API}/${id}` );
+
+  useEffect(() => {
     getPokemonModel(`${API}/${id}`)
   }, [id])
+
+  // console.log(pokemonStats);
   return (
-    <div>PokemonModel</div>
+    <div className='pokemon-stat-model'>
+      {pokemonStats.map((stats, index)=>{
+        return <PokemonStats key={index} stats={stats}/>
+      })}
+    </div>
   )
 }
 
