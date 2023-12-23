e4180167b5999557aca94fe1e8f8fcce412fb78d
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-12-23 21:13:33 +0530

    used react rouder dom for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Searchbar from './components/searchbar/searchBar'
 import Pokemons from './components/pokemonList/Pokemons'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PokemonModel from './components/singlepokemonmodel/PokemonModel'
+import "./app.css"
 
 
 const App = () => {
   return (
-    <div>
-      <Searchbar/>
-      <Pokemons/>
+    <div className='home-page'>
+      <Router>
+        <Searchbar />
+        <Routes>
+          <Route path='/' element={<Pokemons />}/>
+          <Route path='/:id' element={<PokemonModel />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
