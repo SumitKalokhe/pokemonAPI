7f8e280c805a8ed214f1ba3bf6531967a691cae7
Sumit Kalokhe <sumitkalokhe1111@gmail.com>
2023-12-23 21:16:15 +0530

    created page to display pokemon cards

--- src/components/pokemonList/Pokemons.jsx
+++ src/components/pokemonList/Pokemons.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react'
 import { PokemonContext } from '../context/PokemonContext'
 import PokemonList from './PokemonList'
+import "./pokemonlist.css"
 
 const Pokemons = () => {
 
   const { pokemons } = useContext(PokemonContext)
+
+  // console.log(pokemons);
+
   return (
     <div className='pokemon-card-grid'>
       {pokemons.map((pokemon, index) => {
         const imgUrl = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${index+1}.svg`
-        return <PokemonList key={index} {...pokemon} index={index} img={imgUrl} />
+        return <PokemonList key={index} {...pokemon} img={imgUrl} stats={pokemon.details.stats} types={pokemon.details.types} id={index+1}/>
       })}
     </div>
   )
